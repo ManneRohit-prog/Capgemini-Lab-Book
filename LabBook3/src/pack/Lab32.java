95f7248856b3f6fe079605f54e60e71f5f7316ad
ManneRohit-prog <79071515+ManneRohit-prog@users.noreply.github.com>
2021-02-15 10:18:47 +0530

    Add files via upload

---
+++ LabBook3/src/pack/Lab32.java
@@ -0,0 +1,18 @@
+package pack;
+
+import java.util.Scanner;
+
+public class Lab32 {
+
+   public static void main(String[] args) {
+	   Scanner sc = new Scanner(System.in);
+	   String a = sc.nextLine();
+	   String b[] = a.split(" ");
+	   int x = b.length;
+	   String ans = "";
+	   for(int i = 0;i<x;i++) {
+		   ans+=b[i];
+	   }
+	   System.out.println(ans.length());
+   }   
+}
---
+++ LabBook3/src/pack/Lab36.java
@@ -0,0 +1,21 @@
+package pack;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Lab36 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		char a[] = str.toCharArray();
+		int num = str.length();
+		Arrays.sort(a);
+		String k = new String(a);
+		if (str.equals(k)) {
+			System.out.println(true);
+		}
+		else {
+			System.out.println(false);
+		}
+	}
+}
